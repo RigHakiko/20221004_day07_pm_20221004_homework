9ed0240d3c5583e1619300f37e6bd5e4811bcb49
RigHakiko <add-122@hotmail.com>
2022-10-04 21:54:19 +0900

    Initial commit

---
+++ src/main/java/com/group6/mp1/Day07Pm20221004HomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.group6.mp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day07Pm20221004HomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day07Pm20221004HomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group6/mp1/Day07Pm20221004HomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group6.mp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day07Pm20221004HomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
